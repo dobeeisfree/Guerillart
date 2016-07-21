dc788fcc48af8a7a207fbd924f4aee575175f99e
Socool <hi.seokhowon@gmail.com>
2016-07-22 02:25:02 +0900

    DB 업데이트

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20160720080434) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "level"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
