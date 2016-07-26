b3220c673f6f7d12db11e27e58b5c971ecf2a37e
hoon <hoonti06@naver.com>
2016-07-26 23:01:57 +0900

    라인 정리

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,9 @@ class HomeController < ApplicationController
       # redirect_to '/users/sign_in'
     # end
     
-    
       #로그인 상태 확인 테스트 코드
     # if user_signed_in?
       # redirect_to '/users/sign_out'
     # end
-    
-
   end
 end
