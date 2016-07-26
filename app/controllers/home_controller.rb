1ef02b66e51f26bfda7a7d2a7143d0c9a71722f2
hoon <hoonti06@naver.com>
2016-07-26 12:38:44 +0900

    Sign up modal 비밀번호 확인 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
     # unless user_signed_in?
       # redirect_to '/users/sign_in'
     # end
+    # if user_signed_in?
+      # redirect_to '/users/sign_out'
+    # end
+    
+
   end
 end
