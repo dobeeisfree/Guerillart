899962f192f3fe1af704e4ebb1c619f1daa138c8
hoon <hoonti06@naver.com>
2016-08-01 21:54:06 +0900

    지도 마커 찍기 추가

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+show = Show.new
+show.location_x = 37.519695589572116
+show.location_y = 127.04665961216438
+show.save
