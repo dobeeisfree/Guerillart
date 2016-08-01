884132dce6e10adce40b2e5e71231965e2c609ce
Socool <hi.seokhowon@gmail.com>
2016-08-01 16:57:27 +0900

    조금 고침, 앞으로 더 고칠거임

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def dashboard
+  end
+
+  def myguerillart
+  end
+
+  def myguerillart_category
+  end
+
+  def profile
+  end
+
+  def show
+  end
+
+  def write
+  end
+
+  def write_complete
+  end
+
+  def edit
+  end
+
+  def edit_complete
+  end
+
+  def delete_complete
+  end
+end
