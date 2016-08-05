e10a9710eb136f8af2bbe5f7dfdf30a4458850b1
hoon <hoonti06@naver.com>
2016-08-05 21:25:07 +0900

    seed에 장르 추가/ infowindow에 장르 출력

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 show = Show.new
+show.genre = "rock"
 show.location_x = 37.519695589572116
 show.location_y = 127.04732480000007
 show.save #상
 
 show = Show.new
+show.genre = "dance"
 show.location_x = 37.5172363
 show.location_y = 127.05362262676704
 show.save #우
 
 show = Show.new
+show.genre = "hiphop"
 show.location_x = 37.519695589572116
 show.location_y = 127.05362262676704
 show.save #우상
 
 show = Show.new
+show.genre = "juggle"
 show.location_x = 37.5172363
 show.location_y = 127.04732480000007
 show.save #중
 
 show = Show.new
+show.genre = "magic"
 show.location_x = 37.5172363
 show.location_y = 127.04732480000007 - 0.0063
 show.save #좌
 
 show = Show.new
+show.genre = "comedy"
 show.location_x = 37.5172363 - 0.00246
 show.location_y = 127.04732480000007
 show.save #하
