bcd1307bca7c6881bad7ad2b4ad3d86b1e56c66a
hoon <hoonti06@naver.com>
2016-08-11 02:27:51 +0900

    searching controller getBounds 추가/ application.html.erb에서 data-turbolingks-track 비활성화/ routes에 post searching/getBounds 추가

--- app/controllers/searching_controller.rb
+++ app/controllers/searching_controller.rb
@@ -4,4 +4,24 @@ class SearchingController < ApplicationController
     @show_all = Show.all
     
   end
+  
+  def getBounds
+    @neLat = params[:ne_lat]
+    @neLng = params[:ne_lng]
+    @swLat = params[:sw_lat]
+    @swLng = params[:sw_lng]
+    puts @swLng.to_f
+    
+    # Show.reindex
+    @search = Show.search do
+      all_of do
+        # # with(:location_x, 37.514..130.0)
+        # # with(:location_y, 127.05362262676700..130.0)
+        with(:location_x, @swLat.to_f..@neLat.to_f)
+        with(:location_y, @swLng.to_f..@neLng.to_f)
+      end
+    end
+    # @results = @search.results
+    redirect_to :back
+  end
 end
