125eb7080d2fe8663f379d1210eaba2344061b4e
hoon <hoonti06@naver.com>
2016-08-11 18:44:59 +0900

    getBounds controller에서 bounds안에 있는 공연 검색 기능 추가

--- app/controllers/searching_controller.rb
+++ app/controllers/searching_controller.rb
@@ -2,26 +2,46 @@ class SearchingController < ApplicationController
   def index
     @location = params[:location]
     @show_all = Show.all
-    
   end
   
   def getBounds
-    @neLat = params[:ne_lat]
-    @neLng = params[:ne_lng]
-    @swLat = params[:sw_lat]
-    @swLng = params[:sw_lng]
-    puts @swLng.to_f
+    # # @neLat = params[:ne_lat]
+    # # @neLng = params[:ne_lng]
+    # # @swLat = params[:sw_lat]
+    # # @swLng = params[:sw_lng]
+    # # puts @swLng.to_f
+#     
+    # # Show.reindex
+    # @search = Show.search do
+      # all_of do
+        # # # with(:location_x, 37.514..130.0)
+        # # # with(:location_y, 127.05362262676700..130.0)
+        # with(:location_x, @swLat.to_f..@neLat.to_f)
+        # with(:location_y, @swLng.to_f..@neLng.to_f)
+      # end
+    # end
+    # # @results = @search.results
+    # redirect_to :back
     
-    # Show.reindex
+    
+    # @neLat = params[:ne_lat].to_s.to_f
+    # puts @neLat
+    
+    
+    Show.index
     @search = Show.search do
       all_of do
-        # # with(:location_x, 37.514..130.0)
-        # # with(:location_y, 127.05362262676700..130.0)
-        with(:location_x, @swLat.to_f..@neLat.to_f)
-        with(:location_y, @swLng.to_f..@neLng.to_f)
+        with(:location_x, params[:sw_lat].to_s.to_f..params[:ne_lat].to_s.to_f)
+        with(:location_y, params[:sw_lng].to_s.to_f..params[:ne_lng].to_s.to_f)
       end
     end
-    # @results = @search.results
+    @results = @search.results
+    puts Time.now
+    @results.each do |x|
+      puts x.genre 
+    end
+    
     redirect_to :back
+    
   end
 end
