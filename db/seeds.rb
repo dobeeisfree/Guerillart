f5bea35d8c8b41e909f2de942fc3362d58920131
hoon <hoonti06@naver.com>
2016-08-19 16:28:05 +0900

    seed 공연에 user_id 추가

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,48 +9,56 @@ show = Show.new
 show.genre = "balad"
 show.location_x = 37.49720818812354
 show.location_y = 126.95616475134287
+show.user_id = 1
 show.save #학교 운동장
 
 show = Show.new
 show.genre = "jazz"
 show.location_x = 37.53195287306739
 show.location_y = 127.1480328738189
+show.user_id = 2
 show.save #우리집 근처
 
 show = Show.new
 show.genre = "rock"
 show.location_x = 37.519695589572116
 show.location_y = 127.04732480000007
+show.user_id = 3
 show.save #상
 
 show = Show.new
 show.genre = "dance"
 show.location_x = 37.5172363
 show.location_y = 127.05362262676704
+show.user_id = 4
 show.save #우
 
 show = Show.new
 show.genre = "hiphop"
 show.location_x = 37.519695589572116
 show.location_y = 127.05362262676704
+show.user_id = 5
 show.save #우상
 
 show = Show.new
 show.genre = "juggle"
 show.location_x = 37.5172363
 show.location_y = 127.04732480000007
+show.user_id = 6
 show.save #중
 
 show = Show.new
 show.genre = "magic"
 show.location_x = 37.5172363
 show.location_y = 127.04732480000007 - 0.0063
+show.user_id = 7
 show.save #좌
 
 show = Show.new
 show.genre = "comedy"
 show.location_x = 37.5172363 - 0.00246
 show.location_y = 127.04732480000007
+show.user_id = 8
 show.save #하
 
 
