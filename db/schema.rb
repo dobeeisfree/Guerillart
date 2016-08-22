7ba0611d285175d98ea039ecbb4a0025b8a7ffde
hoon <hoonti06@naver.com>
2016-08-22 18:25:35 +0900

    schema 수정

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 20160818071656) do
     t.string   "time"
     t.integer  "status",       default: 0
     t.integer  "creator_id"
-    t.integer  "user_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
