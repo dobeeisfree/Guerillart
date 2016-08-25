822bc549d454ea6d0e4b6ebb6720be0f9003a0ad
dobeeisfree <jubee0124@gmail.com>
2016-08-25 16:24:28 +0900

    시멘틱유아이 재설치

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-// Loads all Semantic javascripts
-//= require semantic-ui
-//= require semantic-ui/modal
-//= require semantic-ui/dropdown
