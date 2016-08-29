bdfa0068d2356e9b19d92a2efe0a46ac22438e0b
hoon <hoonti06@naver.com>
2016-08-29 18:29:23 +0900

    로그인 성공시 새로고침, 실패시 오류 메시지 출력

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,17 +14,29 @@
 //= require jquery_ujs
 //= require_tree .
 $(document).on('submit', '#login_form', function(e) {
-            //do stuff here
-            console.log("hi : "+ e);
-            
+    //do stuff here
+    console.log("hi : "+ e);
 }).on('ajax:success', '#login_form', function(e, data, status, xhr) {
-        //do stuff here 
-        console.log("hi1 : " +JSON.stringify(data) + ", @@@ "+JSON.stringify(status)+ ", @@@ "+ JSON.stringify(xhr));
-        // console.log("hi1 : "+data);
-        // console.log("hi1 : ");
+    //do stuff here 
+    console.log("hi1 : " +JSON.stringify(data) + ", @@@ "+JSON.stringify(status)+ ", @@@ "+ JSON.stringify(xhr));
+    
+    $('.ui.basic.modal.signin').modal('hide');
+    window.location.reload(true);
+    
+    // console.log("hi1 : "+data);
+    // console.log("hi1 : ");
 }).on('ajax:error', '#login_form', function(e, data, status, xhr) {
-        //do stuff here
-        console.log("hi2");
+    //do stuff here
+    console.log("hi2 : " +JSON.stringify(data) + ", @@@ "+JSON.stringify(status)+ ", @@@ "+ JSON.stringify(xhr));
+    console.log(""+data.responseText);
+    $('.footer#login_modal_footer').html('<div class="ui error message"><div class="header" id="login_error_msg">로그인 실패</div><p>' + data.responseText + '</p></div>');
+    //$('#login_modal_footer').innerHTML = '<div class="ui error message"><div class="header" id="login_error_msg">로그인 실패</div><p>' + data.responseText + '</p></div>';
+    
+    
+    
+    // document.getElementById('start_signin_modal').onclick = function() {
+	  // $('.ui.basic.modal.signin').modal('hide');
+	// };
 });
 
 
@@ -38,3 +50,5 @@ $(document).on('submit', '#register_form', function(e){
        //do stuff here
        console.log("hi5");
 });
+
+
