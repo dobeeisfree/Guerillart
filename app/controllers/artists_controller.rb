00581e5f879b76241b413b6ce53b163408c82ffb
dobeeisfree <jubee0124@gmail.com>
2016-09-24 16:33:47 +0900

    artist 컨트롤러 scss, js 정리 #95

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,28 +1,27 @@
 class ArtistsController < ApplicationController
   before_action :authenticate_user!, :only => [ :new, :create, :edit, :update ]
   # before_action :isSignin, :only => [ :new, :create, :edit, :update ]
-  
+
   # def isSignIn
     # unless user_signed_in?
       # redirect_to '/'
     # end
   # end
-  
-  
+
+
   def index
-    @artist_name = params[:artist_name]
-    @users = User.find_by(artist_name: @artist_name)
+    @user = User.all
   end
-  
+
   def new
     if current_user.artist_name.present?
       redirect_to '/dashboard/home'
     end
   end
-  
+
   def create
     if current_user.artist_name.nil?
-      
+
       current_user.artist_name = params[:name]
       current_user.genre = params[:genre]
       current_user.phone_number = params[:phone_number]
@@ -36,23 +35,23 @@ class ArtistsController < ApplicationController
       redirect_to '/dashboard/home'
     end
   end
-  
+
   def edit
    a = params[:artist_name]
    if a != current_user.artist_name
       redirect_to "/artists"
    end
-   
+
    @users = User.find_by(artist_name: a)
    @posts = Post.all
   end
-  
+
   def update
     @users = User.find(params[:user_id])
     if @users.id != current_user.id
       redirect_to "/artists"
     end
-    
+
     @users.introduction = params[:introduction]
     @users.sns = params[:sns]
     @users.image = params[:image]
@@ -64,14 +63,14 @@ class ArtistsController < ApplicationController
     redirect_to :back
     end
   end
-  
+
   def page
     @users = User.find_by_artist_name(params[:artist_name])
-    @one_post = Post.find_by_user_id(@users.id)
+    @one_post = Post.find_by_user_id(@users)
     @posts = Post.all
     @followers = User.all
   end
-  
+
   def search
     @user = User.find_by(artist_name: params[:artist_name])
   end
